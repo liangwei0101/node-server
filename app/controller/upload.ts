ef3f52812e0766c87cbcdb8569f4f832ad320625
liangwei0101 <liangwei0101@outlook.com>
2019-10-24 18:25:40 +0800

    增加异步写的库

--- app/controller/upload.ts
+++ app/controller/upload.ts
@@ -1,5 +1,7 @@
 import { Controller } from 'egg';
-// import { createWriteStream } from 'fs'
+import { createWriteStream } from 'fs';
+const sendToWormhole = require('stream-wormhole');
+const awaitWriteStream = require('await-stream-ready').write;
 
 export default class UploadController extends Controller {
 
@@ -7,6 +9,23 @@ export default class UploadController extends Controller {
     const { ctx } = this;
 
     const stream = await ctx.getFileStream();
+
+    // 路径 + 名称
+    const filename = 'E:/Test/' + stream.filename;
+
+    const writeStream = createWriteStream(filename);
+
+    stream.pipe(writeStream)
+
+    try {
+      //异步把文件流 写入
+      await awaitWriteStream(stream.pipe(writeStream));
+    } catch (err) {
+      //如果出现错误，关闭管道
+      await sendToWormhole(stream);
+      throw err;
+    }
+
     ctx.body = stream.filename
   }
 
